e5495606a8cf3a44f0b13a792630db283e48c878
Lawrence Park <swlpark@gmail.com>
2016-02-21 14:00:28 -0600

    counters.h

---
+++ simplescalar-a1/ir_counters.h
@@ -0,0 +1,18 @@
+extern counter_t sim_reg_nmod_wr;
+extern counter_t sim_reg_uref_wr;
+extern counter_t sim_inef_br;
+extern counter_t sim_transitive_ineff;
+
+extern counter_t sim_mem_nmod_wr;
+extern counter_t sim_mem_uref_wr;
+
+extern md_addr_t _mem_load_addr_0;
+extern md_addr_t _mem_load_addr_1;
+
+extern md_addr_t _mem_store_addr_0;
+extern md_addr_t _mem_store_addr_1;
+
+extern word_t _mem_store_new_word_0;
+extern word_t _mem_store_new_word_1;
+extern word_t _mem_store_old_word_0;
+extern word_t _mem_store_old_word_1;
